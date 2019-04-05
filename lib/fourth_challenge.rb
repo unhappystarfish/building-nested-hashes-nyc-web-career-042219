ccb9dfb75bcc9cde7eec208b50b060a1f9243ec3
Jake L <jakelovitz@gmail.com>
2019-04-05 17:56:50 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,7 +6,7 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         benvolio = {name: "Benvolio", age: "17", attitude: "worried"},
-        mercutio = {name: "Mercutio", age: "18", attitude: "hot-headed"
+        mercutio = {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
    }, 
    :capulet => {
